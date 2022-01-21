0453ef417f6366c3c651ec1f51cd5bc0cb4ba2c7
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-01-21 21:22:21 +0900

    first commit

---
+++ printf/printf_max_len.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+	int	fd;
+	ssize_t rc;
+	size_t len;
+	char	buf[10000];
+
+	fd = open("max_len_test", O_RDONLY);
+	if (argc != 2)
+	{
+		printf("type buffer size after ./a.out\n");
+		return (-1);
+	}
+	len = atoi(argv[1]);
+	rc = read(fd, buf, len);
+	if (rc == -1)
+		return (-1);
+	buf[rc] = '\0';
+	printf("rc is %zu\n", rc);
+	printf("BUFSIZ is: %d\n", BUFSIZ);
+	printf("%s\n", buf);
+	close (fd);
+	return (0);
+}
