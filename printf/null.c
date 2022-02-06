bebd0bbf5f54b7cd3c625ca4111120cb40ba9035
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-02-07 08:17:43 +0900

    adding c

--- printf/null.c
+++ printf/null.c
@@ -3,7 +3,14 @@
 
 int main(void)
 {
+	char	c;
 	char *s = NULL;
+	
+	c = '\0';
+	printf(" NULL %c NULL \n", c);
+	write(1, &c, 1);
+	write(1, "\n", 1);
+
 	printf(" NULL %s NULL \n", s);
 	write(1, &s, 1);
 	write(1, "\n", 1);
