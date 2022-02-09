163a282be6dcc59eea18fdf9f05cd1553da26d57
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-02-09 17:31:22 +0900

    experiment

---
+++ printf/intmax.c
@@ -0,0 +1,36 @@
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+/*
+	int i;
+	
+	i = 0;
+	while (0)
+	{
+		printf("%d, ", i);
+		i += 10;
+	}
+*/
+//	printf("return value is: %d\n", printf("%*d\n", INT_MAX - 3, -1));
+//	printf("return value is: %d\n", printf("%*d\n", INT_MAX - 2, -1));
+	printf("return value is: %d\n", printf("%*d\n", INT_MAX - 1, 1));
+//	printf("return value is: %d\n", printf("%*d\n", INT_MAX, 1));
+/*
+	printf("return value is: %d\n", printf("%*d\n", INT_MAX + 1, -1));
+	printf("return value is: %d\n", printf("%*d\n", INT_MAX + 2, -1));
+	printf("return value is: %d\n", printf("%*d\n", INT_MAX + 3, -1));
+	printf("return value is: %d\n", printf("%*d\n", INT_MAX + 4, -1));
+
+	printf("return value is: %d\n", printf("%d\n", INT_MAX - 3));
+	printf("return value is: %d\n", printf("%d\n", INT_MAX - 2));
+	printf("return value is: %d\n", printf("%d\n", INT_MAX - 1));
+	printf("return value is: %d\n", printf("%d\n", INT_MAX));
+	printf("return value is: %d\n", printf("%ld\n", (long)INT_MAX + 1));
+	printf("return value is: %d\n", printf("%ld\n", (long)INT_MAX + 2));
+	printf("return value is: %d\n", printf("%ld\n", (long)INT_MAX + 3));
+	printf("return value is: %d\n", printf("%ld\n", (long)INT_MAX + 4));
+*/
+	return (0);
+}
--- printf/va_copy.c
+++ printf/va_copy.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int ft_printf(const char *fmt, ...);
+
 int main(void)
 {
 	ft_printf("Hello", 1, 2, 3);
@@ -10,8 +12,22 @@ int main(void)
 int ft_printf(const char *fmt, ...)
 {
 	va_list args;
-
+	va_list args2;
+	int i;
+	int j;
 
 	va_start(args, fmt);
-	va_copy(
-
+	va_copy(args2, args);
+	i = va_arg(args2, int);
+	j = va_arg(args, int);
+	printf("args2 is: %d\n", i);
+	printf("args is: %d\n", j);
+	i = va_arg(args2, int);
+	printf("args2 is: %d\n", i);
+	va_copy(args, args2);
+	j = va_arg(args, int);
+	printf("args is: %d\n", j);
+	va_end(args);
+	va_end(args2);
+	return (0);
+}
