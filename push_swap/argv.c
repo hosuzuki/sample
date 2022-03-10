bb9ca685df4e61f3380c0447effab0be7a968c01
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-10 15:20:48 +0900

    1st commit

---
+++ push_swap/argv.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("argv[0] is : %s\n", argv[0]);
+	printf("argv[1] is : %s\n", argv[1]);
+	printf("argv[2] is : %s\n", argv[2]);
+	printf("argv[3] is : %s\n", argv[3]);
+	printf("argv[4] is : %s\n", argv[4]);
+	return (0);
+}
