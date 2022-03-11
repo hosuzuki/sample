c06b73219803c8380da7d85e7b4a5eaf60d65843
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-11 13:01:15 +0900

    1st commit

--- push_swap/quick_sort.c
+++ push_swap/quick_sort.c
@@ -4,27 +4,34 @@
 
 static int	partition(int *sort, size_t left, size_t right)
 {
-	size_t	l;
+//	size_t	l;
 	size_t	r;
 	int		tmp;
 
-	l = left;
+//	l = left;
 	r = right;
-	while (l < r)
+	while (left < r)
+//	while (l < r)
 	{
-		while (sort[l] < sort[left] && l < right) // ???
-			l++;
-		while (sort[r] >= sort[left] && r > left)
+	//	while (sort[l] < sort[left] && l < right) // ???
+		//	l++;
+//		while (sort[r] >= sort[left] && r > left)
+		while (sort[left] <= sort[r] && left < r)
 			r--;
-		if (l >= r)
+//		if (l >= r)
+		if (left >= r)
 			break;
-		tmp = sort[l];
-		sort[l] = sort[r];
+//		tmp = sort[l];
+//		sort[l] = sort[r];
+//		sort[r] = tmp;
+		tmp = sort[left];
+		sort[left] = sort[r];
 		sort[r] = tmp;
 	}
-	if (l == left)
-		return (l + 1);
-	return (l);
+//	if (l == left)
+//		return (l + 1);
+		return (left + 1);
+//	return (l);
 }
 
 static void	quick_sort(int *sort, size_t left, size_t right)
@@ -34,28 +41,48 @@ static void	quick_sort(int *sort, size_t left, size_t right)
 	if(left >= right)
 		return ;
 	part = partition(sort, left, right);
-	quick_sort(sort, left, part - 1);
+//	quick_sort(sort, left, part - 1);
 	quick_sort(sort, part, right);
 }
 
 int main(int argc, char **argv)
 {
-	char *str;
+/*	char *str;
 	int buf[50];
 	int i;
+	int j;
+	int x;
 
 	i = 0;
 	while(i < argc - 1)
 	{
+		x = 0;
+		j = 0;
 		str = argv[i + 1];
-		if (isdigit(str[0]) != 0)
-			buf[i] = atoi(argv[i + 1]);
-		else
+		while (isdigit(str[x]) != 0)
+			j = j * 10 + (str[x++] - '0');
+	else
 			return (1);
-		i++;
+		buf[i] = j;
+	 	i++;
 	}
 //	buf[i - 1] = '\0';
-	quick_sort(buf, 0, i);
+*/
+
+	int buf[50];
+	int i;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		buf[i] = atoi(argv[i + 1]);
+		if (buf[i++] == 0)
+		{
+			printf("Invalid Input\n");
+			return (1);
+		}
+	}
+	quick_sort(buf, 0, i - 1);
 	for (int j = 0; j < argc - 1; j++)
 		printf("%d ", buf[j]);
 	printf("\n");
