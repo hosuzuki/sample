09d9a3a7961d650ab722b52ce959216f816b8717
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-19 23:05:17 +0900

    adding header

--- exam02/gnl/get_next_line.h
+++ exam02/gnl/get_next_line.h
@@ -4,17 +4,18 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-# define GOOD 1
-# define ERROR -1
-# define END 0
+//# define GOOD 1
+//# define ERROR -1
+//# define END 0
+//# define BUFSIZE 1024
 
-typedef struct s_stock
+typedef struct s_lst
 {
 //	int				fd;
 	char	*str;
 	int	len;
-	struct s_stock	*next;
-}	t_stock;
+//	struct s_stock	*next;
+}	t_lst;
 
 int	get_next_line(char **line);
 
