2ad4ff57f3fe2b2a89bf3192c75e81593f6575be
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-19 22:41:52 +0900

    1st commit

--- exam02/gnl/test.sh
+++ exam02/gnl/test.sh
@@ -1,6 +1,8 @@
-gcc -Wall -Werror -Wextra gnl.c main.c -o get_next_line
-./get_next_line < gnl.c > yours_.res
+gcc -Wall -Werror -Wextra get_next_line.c main.c -o get_next_line -g
+#<<out
+./get_next_line < get_next_line.c > yours_.res
 cat -e yours_.res > yours.res
-cat -e < gnl.c > original.res
+cat -e < get_next_line.c > original.res
 diff -y --suppress-common-line original.res yours.res
 rm -rf original.res yours_.res yours.res get_next_line
+#out
