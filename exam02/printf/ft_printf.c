4965bb3ef5a60e3cf3e404b1eb8b0652ac8fc833
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-21 12:47:01 +0900

    hex is attached

--- exam02/printf/ft_printf.c
+++ exam02/printf/ft_printf.c
@@ -1,24 +1,35 @@
+#include "ft_printf.h"
+
 void ft_speci(const char *fmt, t_lst *lst, int *i)
 {
-	if (fmt[*i] == 's')
-		ft_deci(fmt, lst, i);
-	else if (fmt[*i] == 'x')
-		ft_hex(fmt, lst, i);
-	else if (fmt[*i] == 's')
+	if (fmt[*i] == 'd')
+	{
+		ft_deci(lst, va_arg(lst->args, int));
+		(*i)++;
+	}
+ 	else if (fmt[*i] == 'x')
+	{
+		ft_hex(lst, va_arg(lst->args, unsigned int));
+		(*i)++;
+	}
+/*	else if (fmt[*i] == 's')
+	{
 		ft_str(fmt, lst, i);
-}
+		(*i)++;
+	}
+*/}
 
-	void	ft_save_flags(const char *fmt, t_lst *lst, int *i)
+void	ft_save_flags(const char *fmt, t_lst *lst, int *i)
 {
-	while (ft_isdigit(fmt[*i])
-		lst->wid = lst->wid * 10 + (fmt[*i++] - '0');
+	while (ft_isdigit(fmt[*i]))
+		lst->wid = lst->wid * 10 + (fmt[(*i)++] - '0');
 	if (fmt[*i] == '.')
 	{
-		*i++;
-		while (ft_isdigit(fmt[*i]));
-			lst->pre = lst->pre * 10 + (fmt[*i++] - '0');
-		if (lst->pre = 0)
-			lst->pre = 1); // check
+		(*i)++;
+		while (ft_isdigit(fmt[*i]))
+			lst->pre = lst->pre * 10 + (fmt[(*i)++] - '0');
+		if (lst->pre == 0)
+			lst->pre = 1; // check
 	}
 }
 
@@ -35,17 +46,27 @@ int	ft_print_out(const char *fmt, t_lst *lst)
 	while(fmt[i])
 	{
 		ft_init_lst(lst);
-		if (fmt[*i] == '%')
+		if (fmt[i] == '%')
 		{
-			if (fmt[*i + 1] == '%')
-				lst->len += ft_write(1, "%", 1);
-			ft_save_flags(fmt, lst, &i);
-			ft_speci(fmt, lst, &i);
-			if 
+			i++;
+			if (fmt[i] == '%')
+			{
+				ft_write_char(lst, '%');
+				i++;
+			}
+			else
+			{
+				ft_save_flags(fmt, lst, &i);
+				ft_speci(fmt, lst, &i);
+			}
 		}
 		else
-		
-		
+			ft_write_char(lst, fmt[i++]);
+		if (lst->status == 1)
+			return (-1);
+	}
+	return ((int)lst->len);
+}
 
 int	ft_printf(const char *fmt, ...)
 {
@@ -59,6 +80,7 @@ int	ft_printf(const char *fmt, ...)
 		return (-1);
 	va_start(lst->args, fmt);
 	lst->len = 0;
+	lst->status = 0;
 	len = ft_print_out(fmt, lst);
 	va_end(lst->args);
 	free (lst);
--- exam02/printf/utils.c
+++ exam02/printf/utils.c
@@ -1,12 +1,90 @@
+#include "ft_printf.h"
+
 int	ft_isdigit(int	c)
 {
 	return ('0' <= c && c <= '9');
 }
 
-int ft_write(t_lst *lst, char *str)
+size_t ft_strlen(char *str)
 {
-	if ( -1 = ft_intmax(lst, str));
+	size_t i = 0;
+	if (!str)
 		return (-1);
+	while (str[i])
+		i++;
+	return (i);
+}
 
-	return(write(1, &str, ft_strlen(str));
+void ft_write_str(t_lst *lst, char *str)
+{
+	size_t total;
+	total = lst->len + ft_strlen(str);
+	if (total > INT_MAX)
+	{
+		lst->status = -1;
+		return ;
+	}
+	lst->len += write(1, str, ft_strlen(str));
+}
+
+void ft_write_char(t_lst *lst, int c)
+{
+	size_t total;
+	total = lst->len + 1;
+	if (total > INT_MAX)
+	{
+		lst->status = -1;
+		return ;
+	}
+	lst->len += write(1, &c, 1);
+}
+
+void	ft_itoa(char *deci, int d, int *i)
+{
+
+	if (d == INT_MIN)
+	{
+		ft_itoa(deci, d /10, i);
+		deci[(*i)++] = '8';
+	}
+	else if (d < 0)
+	{
+		deci[(*i)++] = '-';
+		ft_itoa(deci, -d, i);
+	}
+	else if (9 < d)
+	{
+		ft_itoa(deci, d / 10, i);
+		ft_itoa(deci, d % 10, i);
+	}
+	else
+		deci[(*i)++] = d + '0';
+}
+
+void ft_deci(t_lst *lst, int d)
+{
+	char deci[15] = {'\0'};
+	int i = 0;
+
+	ft_itoa(deci, d, &i);
+	ft_write_str(lst, deci);
+}
+
+char *ft_dtoh(unsigned int h, char *base, size_t *i)
+{
+	static char res[10] = {'\0'};
+
+	if (16 <= h)
+		ft_dtoh(h / 16, base, i);
+	res[(*i)++] = base[h % 16];
+	return (res);
+}
+
+void ft_hex(t_lst *lst, unsigned int h)
+{
+	size_t i = 0;
+	char *hex;
+
+	hex = ft_dtoh(h, "0123456789abcdef", &i);
+	ft_write_str(lst, hex);
 }
