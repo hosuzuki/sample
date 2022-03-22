ae741bef8d66b9f406635a20a35948177ff3b447
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-22 13:23:24 +0900

    checking width and pre

---
+++ printf/error.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+/*
+ 	printf("%5.2d\n", -1);
+ 	printf("%5.3d\n", -1);
+	printf("%5.4d\n", -1);
+ 	printf("%5.5d\n", -1);
+ 	printf("%5.6d\n", -1);
+*/
+	printf("%5.1d\n", 12345);
+ 	printf("%5.1x\n", 12345);
+ 	printf("%5.1s\n", "hello");
+ 	printf("%-5s\n", "hi");
+ 	printf("%-5s\n", "hi");
+ 	printf("%-5s\n", "hi");
+/*
+ 	printf("%");
+	printf("\n");
+	printf("%5");
+	printf("\n");
+	printf("%05");
+*/
+	return (0);
+}
+
