755b8b32cb71af4d261ab34c095134e4b0e5cb79
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-24 18:07:43 +0900

    1st commit

--- fract/srcs/main.c
+++ fract/srcs/main.c
@@ -1,125 +1,144 @@
-#include "fract.h"
+#include	"fract.h"
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<math.h>
 
-#include <stdio.h>
-#include <stdlib.h>
+#define		X_EVENT_KEY_PRESS		2
+#define		X_EVENT_KEY_EXIT		17
+#define		X_EVENT_MOUSE_PRESS		4
+#define		X_EVENT_MOUSE_MOTION	6
+#define		WIN_WIDTH				800
+#define		WIN_HEIGHT				600
+#define		KEY_ESC					53
+#define		ITER_MAX				100
 
-void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+typedef	struct	s_img{
+	void		*img_ptr;
+	char		*data;
+	int			size_l;
+	int			bpp;
+	int			endian;
+}				t_img;
+
+typedef	struct	s_mlx{
+	void		*mlx_ptr;
+	void		*win;
+	t_img		img;
+}				t_mlx;
+
+int		mandelbrot(int count_w, int count_h, int iter)
+{
+	double c_re;
+	double c_im;
+	double x;
+	double x_new;
+	double y;
+
+	c_re = ((count_w - WIN_WIDTH / 2) * 3.0 / WIN_WIDTH) - 0.5;
+	c_im = ((WIN_HEIGHT / 2) - count_h) * 2.0 / WIN_HEIGHT;
+	x = 0;
+	y = 0;
+	while ((pow(x, 2.0) + pow(y, 2.0) < 4) && (iter < ITER_MAX))
+	{
+		x_new = pow(x, 2.0) - pow(y, 2.0) + c_re;
+		y = 2 * x * y + c_im;
+		x = x_new;
+		iter++;
+	}
+	return (iter);
+}
+
+int		julia(int count_w, int count_h, int iter, t_img *img)
+{
+	double	c_re;
+	double	c_im;
+	double	x;
+	double	x_new;
+	double	y;
+
+	(void)img;
+	c_re = -0.75;
+	c_im = 0;
+	x = ((count_w - WIN_WIDTH / 2) * 4.0 / WIN_WIDTH);
+	y = ((WIN_HEIGHT / 2) - count_h) * 4.0 / WIN_HEIGHT;
+	while ((pow(x, 2.0) + pow(y, 2.0) < 4) && (iter < ITER_MAX))
+	{
+		x_new = pow(x, 2.0) - pow(y, 2.0) + c_re;
+		y = 2 * x * y + c_im;
+		x = x_new;
+		iter++;
+	}
+	return (iter);
+}
+
+int		window_init(t_mlx *mlx)
+{
+	if (!(mlx->mlx_ptr = mlx_init()))
+		return (0);
+	if (!(mlx->win = mlx_new_window(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "A simple example")))
+		return (0);
+	if (!(mlx->img.img_ptr = mlx_new_image(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT)))
+		return (0);
+	if (!(mlx->img.data = mlx_get_data_addr(mlx->img.img_ptr, &mlx->img.bpp, &mlx->img.size_l, &mlx->img.endian)))
+		return (0);
+	return (1);
+}
+
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel /8));
+	dst = img->data + (x * img->bpp / 8) + (y * img->size_l);
 	*(unsigned int *)dst = color;
 }
 
-void	press_esc_key(int key, void *p)
+void	put_pixel(t_img *img)
 {
-	(void)p;
-	if (key == 0xFF1B)
+	int		iter;
+	int		count_w;
+	int		count_h;
+
+	count_h = -1;
+	while (++count_h <= WIN_HEIGHT)
+	{
+		count_w = -1;
+		while (++count_w <= WIN_WIDTH)
+		{
+			iter = mandelbrot(count_w, count_h, 0);
+			//iter = julia(count_w, count_h, 0, img);
+			if (iter < ITER_MAX)
+				my_mlx_pixel_put(img, count_w, count_h, 0x00FFFFFF);
+			else
+				my_mlx_pixel_put(img, count_w, count_h, 0x00000000);
+		}
+	}
+}
+
+int		key_press(int keycode)
+{
+	if (keycode == KEY_ESC)
 		exit(0);
+	else
+		return (0);
+	return(0);
 }
 
-void	key_check(int key,void *p)
+int		close(int keycode)
 {
-	(void)p;
-  printf("Key in Win : %d\n",key);
-  if (key==0xFF1B)
-    exit(0);
+	(void)keycode;
+	exit(0);
 }
 
-int main()
+int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_mlx	mlx;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx_test");
-	mlx_key_hook(win_ptr, press_esc_key, win_ptr);
-	mlx_loop(mlx_ptr);
+	if (!window_init(&mlx))
+		return (0);
+	put_pixel(&mlx.img);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img.img_ptr, 0, 0);
+	mlx_hook(mlx.win, X_EVENT_KEY_PRESS, 0, key_press, 0);
+	mlx_hook(mlx.win, X_EVENT_KEY_EXIT, 0, close, 0);
+	mlx_loop(mlx.mlx_ptr);
+	return (0);	
 }
-/*
-int main(void)
-{
-  void	  *mlx;
-  void	  *mlx_win;
-  t_data  img;
-  int     i;
-  int     j;
-  int     k;
-
-  mlx = mlx_init();
-  mlx_win = mlx_new_window(mlx, 600, 400, "Triangle");
-  img.img = mlx_new_image(mlx, 600, 400);
-  img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-  for (i = 200, k = 99; i < 300; i++, k -= 1)
-  {
-    for (j = 150 + k; j < 250; j++)
-      my_mlx_pixel_put(&img, i, j, 0x00FFFF00);
-  }
-  for (i = 300, k = 0; i < 350; i++, k += 2)
-  {
-    for (j = 150 + k; j < 250; j++)
-	     my_mlx_pixel_put(&img, i, j, 0x000000FF);
-  }
-  for (i = 200, k = 99; i < 300; i++, k -= 1)
-  {
-    for (j = 350 - k; j > 250 ; j--)
-      my_mlx_pixel_put(&img, i, j, 0x000000FF);
-  }
-  for (i = 300, k = 0; i < 350; i++, k += 2)
-  {
-    for (j = 350 - k; j > 250; j--)
-	     my_mlx_pixel_put(&img, i, j, 0x00FFFF00);
-  }
-  mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-  mlx_loop(mlx);
-  return (0);
-}
-*/
-
-/*
-int create_trgb(int t, int r, int g, int b)
-{
-	return (t << 24 | r << 16 | g << 8 | b);
-}
-
-int	get_t(int trgb)
-{
-	return ((trgb >> 24) & 0xFF);
-}
-
-int get_r(int trgb)
-{
-	return ((trgb >> 16) & 0xFF);
-}
-
-int get_g(int trgb)
-{
-	return ((trgb >> 8) & 0xFF);
-}
-
-int get_b(int trgb)
-{
-	return (trgb & 0xFF);
-}
-
-int create_trgb(unsigned char t, unsigned char r, unsigned char g, unsigned char b)
-{
-	return (*(int *)(unsigned char [4]{b, g, r t});
-}
-
-int main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img	= mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 105, 105, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
-}
-*/
-
