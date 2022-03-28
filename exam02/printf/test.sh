2c25311f73fdae3736cfc0332ff765517a1718f1
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-28 09:01:24 +0900

    move files to exam03

--- exam02/printf/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm diff.txt pf.txt ft.txt pf.out ft.out
-gcc  -D PRINT="printf" ft_printf.c main.c -o pf.out
-gcc -Wall -Werror -Wextra -D  PRINT="ft_printf"  ft_printf.c main.c -o ft.out -g
-./pf.out >> pf.txt
-./ft.out >> ft.txt
-diff ft.txt pf.txt >> diff.txt
