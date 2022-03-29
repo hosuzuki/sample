5cbbf30f64ebedf1a1c3bf3f8748734c729ec531
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-29 21:58:56 +0900

    updates

--- fract/includes/fract.h
+++ fract/includes/fract.h
@@ -4,13 +4,16 @@
 typedef struct s_data
 {
 	void	*img;
+	void	*mlx;
+	void	*win;
 	char	*addr;
 	int	bits_per_pixel;
 	int	line_length;
 	int	endian;
 }	t_data;
 
-#include <../minilibx-linux/mlx.h>
-
+# include "../minilibx-linux/mlx.h"
+# include "../libft/includes/libft.h"
+//void			ft_putnbr_fd(int n, int fd);
 
 #endif
