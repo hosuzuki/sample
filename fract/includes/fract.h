4610de31b76e52bf8ba12081ca7b2139a8d9a0a6
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-03-30 14:28:38 +0900

    updates

--- fract/includes/fract.h
+++ fract/includes/fract.h
@@ -46,4 +46,14 @@ typedef struct s_rect
 	int color;
 }	t_rect;
 
+enum {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
 #endif
