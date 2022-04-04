ef102bd97778035abc111984cda6ad3d3cfb8341
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-05 00:10:09 +0900

    1st commit

---
+++ exam02/atoi/ft_atoi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	int		res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] <= 32 || str[i] == 127)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			sign = -1;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+int		main(void)
+{
+	printf("%d\n", ft_atoi("123321"));
+	return (0);
+}
