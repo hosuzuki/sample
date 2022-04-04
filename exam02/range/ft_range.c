15281a73a658a3b6c35f110156b252d97fde3446
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-05 00:35:36 +0900

    mod

--- exam02/range/ft_range.c
+++ exam02/range/ft_range.c
@@ -1,7 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int	*ft_range(int start, int end)
+int		ft_abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	return (x);
+}
+
+int		*ft_range(int start, int end)
+{
+	int i;
+	int *tab;
+
+	i = 0;
+	tab = (int*)malloc(sizeof(int) * ft_abs(start - end) + 1);
+	if (start == end)
+		tab[i] = start;
+	else if (start < end)
+	{
+		while (start < end)
+			tab[i++] = start++;
+		tab[i] = start;
+	}
+	else if (start > end)
+	{
+		while (start > end)
+			tab[i++] = start--;
+		tab[i] = start;
+	}
+	return (tab);
+}
+/*
+ int	*ft_range(int start, int end)
 {
 	long long len;
 	int *res;
@@ -22,6 +53,7 @@ int	*ft_range(int start, int end)
 	printf ("i: %d\n", i);
 	return (res);
 }
+*/
 
 #ifdef TEST
 #include <stdio.h>
@@ -31,10 +63,10 @@ int main(void)
 	int *res;
 	int i = 0;
 
-	res = ft_range(-20, -10);
+	res = ft_range(0, -3);
 	if(!res)
 		return (-1);
-	while (i < 15)
+	while (i < 4)
 		printf("%d\n", res[i++]);
 //`	printf("size: %lu\n", sizeof(res) / sizeof(int));
 }
