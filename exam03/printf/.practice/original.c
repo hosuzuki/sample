f86d2c53345b735cd2ee47431fc6d46d17c8e719
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-24 08:08:23 +0900

    1st commit

--- exam03/printf/.practice/original.c
+++ exam03/printf/.practice/original.c
@@ -1,38 +1,39 @@
-void ft_deci(t_lst *lst, int d)
+int	ft_isdigit(int c)
 {
-	char deci[15] = {'\0'};
-	int i = 0;
-	int j = 0;
-	int space = 0;
-	int zero = 0;
-
-	ft_itoa(deci, d, &i);
-	if (d < 0)
-		lst->sign = -1;
-	if (lst->pre < lst->wid)
-	{
-		space = ft_space_deci(lst, i);
-		while (j++ < space)
-			ft_write_char(lst, ' ');
-	}
-	if (lst->sign == -1)
-		ft_write_char(lst, '-');
-	j = 0;
-	if (0 < lst->pre)
-	{
-		zero = ft_zero_deci(lst, i, space);
-		while (j++ < zero)
-			ft_write_char(lst, '0');
-	}
-	if (d == 0 && lst->pre == 0 && lst->pre_flag == 1)
-	{
-		if (0 < lst->wid)
-			 ft_write_char(lst, ' ');
-		else
-			 ft_write_str(lst, "");
-		return ;
-	}
-	ft_write_str(lst, deci);
+	return ('0' <= c && c <= '9');
 }
 
+size_t ft_strlen(char *str)
+{
+	size_t i = 0;
+	if (!str)
+		return (-1);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void ft_write_str(t_lst *lst, char *str)
+{
+	size_t total;
+	total = lst->len + ft_strlen(str);
+	if (total > INT_MAX)
+	{
+		lst->status = -1;
+		return ;
+	}
+	lst->len += write(1, str, ft_strlen(str));
+}
+
+void ft_write_char(t_lst *lst, int c)
+{
+	size_t total;
+	total = lst->len + 1;
+	if (total > INT_MAX)
+	{
+		lst->status = -1;
+		return ;
+	}
+	lst->len += write(1, &c, 1);
+}
 
