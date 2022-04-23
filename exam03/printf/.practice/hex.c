accc6fcd904aabc3a2e899b0ab82ad5037e5c39f
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-23 18:16:17 +0900

    1st commit

--- exam03/printf/.practice/hex.c
+++ exam03/printf/.practice/hex.c
@@ -1,17 +1,18 @@
-void	ft_dtoh(char *res, unsigned int h, char *base, int *i)
+void ft_dtoh(char *base, char *res, unsigned int h, int *i)
 {
+//	if (i <= 16)
 	if (16 <= h)
-		ft_dtoh(res, h / 16, base, i);
+		ft_htoa(base, res, h / 16, i);
 	res[(*i)++] = base[h % 16];
 }
 
 void ft_hex(t_lst *lst, unsigned int h)
 {
-	int i = 0;
 	int len;
+	int i = 0;
 	char res[10] = {'\0'};
 
-	ft_dtoh(res, h, "0123456789abcdef", &i);
+	ft_dtoh("0123456789abcdef", res, h, &i);
 	len = i;
 	if (len < lst->wid)
 		len = lst->wid;
@@ -19,9 +20,10 @@ void ft_hex(t_lst *lst, unsigned int h)
 		len = lst->pre;
 	while (i < len)
 	{
-		if (0 <= lst->pre && len <= lst->pre)
+//		if (lst->pre_flag == 1 && i < lst->pre)
+		if(lst->pre_flag == 1 && len <= lst->pre)
 			ft_write_char(lst, '0');
-		else 
+		else
 			ft_write_char(lst, ' ');
 		len--;
 	}
