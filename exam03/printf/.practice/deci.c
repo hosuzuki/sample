ba326deed659140f60e4a4c086aa56615f92c4b2
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-23 23:44:53 +0900

    1st commit

---
+++ exam03/printf/.practice/deci.c
@@ -0,0 +1,39 @@
+void ft_deci(t_lst *lst, int d)
+{
+	int space;
+	int zero;
+	int i = 0;
+	int j = 0;
+//	char res[10] = {'\0'};
+	char res[15] = {'\0'};
+
+	ft_dtoa(res, d, &i);
+	if (0 > d)
+		lst->sign = -1;
+	if (lst->pre < lst->wid)
+	{
+		space = ft_space(lst, i);
+		while (j++ < space)
+			ft_write_char(lst, ' ');
+	}
+	if (lst->sign == -1)
+		ft_wriet_char(lst, '-');
+	if (0 < lst->pre)
+	{
+		j = 0;
+		zero = ft_zero(lst, i, space);
+		while (j++ < zero)
+	//		ft_write_char(lst, ' ')
+			ft_write_char(lst, '0')
+	}
+	if (d == 0 && lst->pre == 0 && lst->pre_flag = 1)
+	{
+	//	if 
+		if (0 < lst->wid)
+			ft_write_char(lst, ' ');
+		else 
+			ft_write_char(lst, "");
+		return ;
+	}
+	ft_write_str(lst, res);
+}
--- exam03/printf/.practice/original.c
+++ exam03/printf/.practice/original.c
@@ -1,29 +1,38 @@
-void	ft_dtoh(char *res, unsigned int h, char *base, int *i)
+void ft_deci(t_lst *lst, int d)
 {
-	if (16 <= h)
-		ft_dtoh(res, h / 16, base, i);
-	res[(*i)++] = base[h % 16];
-}
-
-void ft_hex(t_lst *lst, unsigned int h)
-{
+	char deci[15] = {'\0'};
 	int i = 0;
-	int len;
-	char res[10] = {'\0'};
+	int j = 0;
+	int space = 0;
+	int zero = 0;
 
-	ft_dtoh(res, h, "0123456789abcdef", &i);
-	len = i;
-	if (len < lst->wid)
-		len = lst->wid;
-	if (len < lst->pre)
-		len = lst->pre;
-	while (i < len)
+	ft_itoa(deci, d, &i);
+	if (d < 0)
+		lst->sign = -1;
+	if (lst->pre < lst->wid)
 	{
-		if (0 <= lst->pre && len <= lst->pre)
-			ft_write_char(lst, '0');
-		else 
+		space = ft_space_deci(lst, i);
+		while (j++ < space)
 			ft_write_char(lst, ' ');
-		len--;
 	}
-	ft_write_str(lst, res);
+	if (lst->sign == -1)
+		ft_write_char(lst, '-');
+	j = 0;
+	if (0 < lst->pre)
+	{
+		zero = ft_zero_deci(lst, i, space);
+		while (j++ < zero)
+			ft_write_char(lst, '0');
+	}
+	if (d == 0 && lst->pre == 0 && lst->pre_flag == 1)
+	{
+		if (0 < lst->wid)
+			 ft_write_char(lst, ' ');
+		else
+			 ft_write_str(lst, "");
+		return ;
+	}
+	ft_write_str(lst, deci);
 }
+
+
