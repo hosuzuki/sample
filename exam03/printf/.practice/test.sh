bfc3687e5949b4d8b92cac44fdc0ac95498f767a
Hokuto Suzuki <81731043+HawkSUZUKI@users.noreply.github.com>
2022-04-26 01:38:16 +0900

    update

--- exam03/printf/.practice/test.sh
+++ exam03/printf/.practice/test.sh
@@ -1,7 +1,10 @@
-rm ft.txt pf.txt
-gcc -Wall -Werror -Wextra ft_printf.c main.c -D PRINT="ft_printf" -g -o ft.out
-gcc -Wall -Werror -Wextra ft_printf.c main.c -D PRINT="printf" -o pf.out
-./ft.out >> ft.txt
+rm ft.txt pf.txt diff.txt
+#forgot main.c
+# check the order
+gcc -Wall -Wextra -Werror ft_printf.c main.c -D PRINT="printf"  -o pf.out
+gcc -Wall -Wextra -Werror ft_printf.c main.c -D PRINT="ft_printf" -o ft.out
 ./pf.out >> pf.txt
-rm ft.out pf.out
-diff -y --suppress-common-line ft.txt pf.txt
+./ft.out >> ft.txt
+rm pf.out ft.out
+diff pf.txt ft.txt >> diff.txt
+#diff -y --suppress-common-line pf.txt ft.txt
